f1ca579075cf759a322740a9cc137e6a2475f464
loxygenK <loxygen.k@gmail.com>
2020-02-09 21:54:21 +0900

    Add the feature to read score data from a text file.

--- Main.py
+++ Main.py
@@ -5,7 +5,92 @@ from pygame.locals import *
 import Romautil
 import pygame_misc
 
+import re
+
 pygame.init()
+
+class Score:
+    LOG_ERROR = 1
+    LOG_WARN = 2
+
+    def __init__(self):
+        self.properties = {}
+        self.log = []
+        self.score = []
+
+
+def set_val_to_dictionary(dict, key, value):
+    if key in dict.keys():
+        dict[key] = value
+    else:
+        dict.setdefault(key, value)
+
+
+def read_score(file_name):
+
+    lines = []
+    with open(file_name, mode="r") as f:
+        lines = f.readlines()
+
+    score = Score()
+
+    re_rect_blacket = re.compile(r"\[(.*)\]")
+
+    score_data = []
+    current_minute = 0
+    current_seconds = 0.0
+
+    is_in_song = False
+    for line in lines:
+        line = line.strip()
+
+        # Comment
+        if line.startswith("#"): continue
+
+        # Blank line
+        if len(line) == 0: continue
+
+        # Song property
+        if line.startswith(":"):
+            line = line[1:]
+            key, value = line.split()
+            set_val_to_dictionary(score.properties, key, value)
+            continue
+
+        rect_blk_match = re_rect_blacket.match(line)
+
+        if rect_blk_match is not None:
+            command = rect_blk_match[1]
+            if command == "start":
+                is_in_song = True
+            elif command == "end":
+                is_in_song = False
+            continue
+
+        if not is_in_song:
+            score.log.append([Score.LOG_WARN, [line, "Unknown text outside song section!"]])
+            score.properties = {}
+            score.score = {}
+            break
+
+        # Minute
+        if line.startswith("|"):
+            line = line[1:]
+            current_minute = int(line)
+            continue
+
+        # Seconds
+        if line.startswith("*"):
+            line = line[1:]
+            current_seconds = float(line)
+            continue
+
+        score.score.append([60 * current_minute + current_seconds, line])
+
+    return score
+
+
+
 
 
 def main():
@@ -18,8 +103,10 @@ def main():
     system_font = pygame.font.Font("mplus-1m-medium.ttf", 16)
 
     # TODO: Read from file
-    target_kana = u"たいへんつかれた"
-    target_roma = Romautil.hira2roma(target_kana)
+    score_data = read_score("test_music_text.tsc")
+
+    return
+    # After here no works well :/
 
     count = 0
     missed = 0
