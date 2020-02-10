889d1b617eb10a6f2c771599914e05eddc072c38
loxygenK <loxygen.k@gmail.com>
2020-02-10 10:27:08 +0900

    [Add] Show full text.

--- Main.py
+++ Main.py
@@ -167,8 +167,9 @@ def main():
     screen = pygame.display.set_mode((600, 480))
     pygame.display.set_caption("Musical Typer")
 
-    nihongo_font = pygame.font.Font("mplus-1m-medium.ttf", 64)
+    nihongo_font = pygame.font.Font("mplus-1m-medium.ttf", 48)
     alphabet_font = pygame.font.Font("mplus-1m-medium.ttf", 32)
+    full_font = pygame.font.Font("mplus-1m-medium.ttf", 24)
     system_font = pygame.font.Font("mplus-1m-medium.ttf", 16)
 
     score_data = read_score("test_music_text.tsc")
@@ -190,9 +191,10 @@ def main():
             screen.fill((0, 0, 0))
 
             pygame_misc.print_str(screen, 5, 0, nihongo_font, target_kana)
-            pygame_misc.print_str(screen, 5, 65, alphabet_font, target_roma)
-            pygame_misc.print_str(screen, 5, 100, system_font, "Typed: {}".format(count))
-            pygame_misc.print_str(screen, 5, 120, system_font, "Miss: {}".format(missed))
+            pygame_misc.print_str(screen, 5, 55, full_font, full, (192, 192, 192))
+            pygame_misc.print_str(screen, 5, 80, alphabet_font, target_roma)
+            pygame_misc.print_str(screen, 5, 130, system_font, "Typed: {}".format(count))
+            pygame_misc.print_str(screen, 5, 150, system_font, "Miss: {}".format(missed))
 
             for event in pygame.event.get():
                 if event.type == QUIT:
