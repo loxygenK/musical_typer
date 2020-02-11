2da20cf280495d1b9eaf1487552632816372fd42
loxygenK <loxygen.k@gmail.com>
2020-02-11 10:32:46 +0900

    [Edit] Very, very big refactoring Maybe i'll do again for better code

---
+++ GameSystem.py
@@ -0,0 +1,242 @@
+import pygame
+import pygame_misc
+import Romautil
+
+class Screen:
+    """
+    画面処理を簡単にするためのクラス。
+    このクラスのインスタンスは画面そのものも持つ
+    """
+
+    nihongo_font = pygame.font.Font("mplus-1m-medium.ttf", 48)
+    alphabet_font = pygame.font.Font("mplus-1m-medium.ttf", 32)
+    full_font = pygame.font.Font("mplus-1m-medium.ttf", 24)
+    system_font = pygame.font.Font("mplus-1m-medium.ttf", 16)
+
+    def __init__(self):
+        self.screen = pygame.display.set_mode((600, 480))
+        pygame.display.set_caption("Musical Typer")
+
+    def print_str(self, x, y, font, text, color=(255, 255, 255)):
+        pygame_misc.print_str(self.screen, x, y, font, text, color)
+
+
+class GameProgressInfo:
+    """
+    ゲームの進行情報を保持するクラス。
+    """
+
+    def __init__(self, score):
+
+        self.lyrincs_index = -1
+        """今いる歌詞のインデックス。 get_current_lyrincsメソッドにより更新される"""
+
+        self.zone_index = -1
+        """今いるゾーンのインデックス。 get_zone_lyrincsメソッドにより更新される"""
+        self.score = score
+
+    def next_lyrincs_incoming(self, pos):
+        """
+        与えられた時間において、次の歌詞が来ているか。
+
+        :param pos: 現在の時間
+        :return: 次の歌詞が来ている場合にTrue
+        """
+        return self.score.score[self.lyrincs_index + 1][0] <= pos
+
+    def next_zone_incoming(self, pos):
+        """
+        与えられた時間に置いて、次のゾーンが来ているか。
+
+        :param pos: 現在の時間
+        :return: 次のゾーンが来ている場合にTrue
+        """
+        return self.score.score[self.zone_index + 1][0] <= pos
+
+    def get_current_lyrincs(self, pos):
+        """
+        与えられた時間に置いて打つべき歌詞のデータを求める。
+
+        :param pos: 現在の時間
+        :return: データ, lyrincs_indexが変化したか
+        """
+
+        # TODO: 処理を軽くする(キャッシュとか使って)
+        for i in range(len(self.score.score)):
+
+
+            if self.score.score[i][0] >= pos:
+
+                is_lidx_changes = (i - 1) != self.lyrincs_index
+                if is_lidx_changes:
+                    self.lyrincs_index = i - 1
+                return self.score.score[i - 1], is_lidx_changes
+
+        return None
+
+    def get_current_zone(self, pos):
+        """
+        与えられた時間が属するゾーンを求める。
+
+        :param pos: 現在の時間
+        :return: ゾーン名。ゾーンに属していない場合はNoneを返す
+        """
+        # TODO: 処理を軽くする(キャッシュとか使って)
+        for i in range(len(self.score.zone)):
+
+            if self.score.zone[i][0] >= pos and self.score.zone[i][2] == "end":
+                if self.score.zone[i - 1][0] <= pos and self.score.zone[i - 1][2] == "start":
+                    return self.score.zone[i][1]
+                else:
+                    return None
+
+        return None
+
+    def get_sentence_full_time(self):
+        """
+        現在の歌詞が表示される時間を求める。
+
+        :return: 現在の歌詞時間。
+        """
+        next_sentence_time = self.score.score[self.lyrincs_index + 1][0]
+        this_sentence_time = self.score.score[self.lyrincs_index][0]
+
+        return next_sentence_time - this_sentence_time
+
+    def get_sentence_elasped_time(self, pos):
+        """
+        現在の歌詞が表示されてから経った時間を求める。
+
+        :param pos: 現在の時間
+        :return: 経った時間。
+        """
+
+        next_sentence_time = self.score.score[self.lyrincs_index + 1][0]
+        return next_sentence_time - pos
+
+    def get_time_remain_ratio(self, pos):
+        """
+        0～1で、どのくらい時間が経ったかを求める。
+
+        :param pos: 現在時間
+        :return: 経った時間を0～1で。
+        """
+        return self.get_sentence_elasped_time(pos) / self.get_sentence_full_time()
+
+
+class GameJudgementInfo:
+    """
+    スコアやタイプ成功回数／失敗回数を管理する。
+    """
+
+    ONE_CHAR_POINT = 10
+    PERFECT_POINT = 100
+    CLEAR_POINT = 50
+    MISS_POINT = -30
+    COULDNT_TYPE_POINT = -2
+
+    def __init__(self):
+        self.target_roma = ""
+        self.target_kana = ""
+        self.full = ""
+
+        self.count = 0
+        self.missed = 0
+
+        self.sent_miss = 0
+        self.sent_count = 0
+
+        self.point = 0
+
+        self.completed = True
+
+
+    def get_sentence_missrate(self):
+        """
+        歌詞ごとの成功比率を求める。
+        成功回数+失敗回数が0の場合は、成功回数を返す。(つまり0になる)
+        :return: 成功比率（成功回数/(成功回数+失敗回数)）
+        """
+
+        trying_sumup = self.sent_miss + self.sent_count
+
+        # To prevent ZeroDivision
+        if trying_sumup == 0:
+            trying_sumup = 1
+
+        return self.sent_count / trying_sumup
+
+    def set_current_lyrinc(self, full, kana):
+        """
+        現在打つべき歌詞を設定する。kanaのローマ字変換結果が0文字だった場合は、self.completed はFalseになる。
+
+        :param full: 歌詞
+        :param kana: 歌詞のふりがな
+        :return: なし
+        """
+
+        self.full = full
+        self.target_kana = kana
+        self.target_roma = Romautil.hira2roma(self.target_kana)
+
+        if len(self.target_roma) == 0:
+            self.completed = True
+
+
+    def reset_sentence_score(self):
+        """
+        歌詞ごとの進捗情報を消去する。
+
+        :return: なし
+        """
+        self.sent_count= 0
+        self.sent_miss = 0
+        self.completed = False
+
+    def count_success(self):
+        """
+        タイプ成功をカウントする。
+
+        :return: このカウントで完了した場合にはTrue、していない場合にはFalse。
+        """
+
+        self.count += 1
+        self.point += GameJudgementInfo.ONE_CHAR_POINT
+        self.sent_count += 1
+
+        self.target_roma = self.target_roma[1:]
+        self.target_kana = Romautil.get_not_halfway_hr(self.target_kana, self.target_roma)
+
+        if len(self.target_roma) == 0:
+            self.point += GameJudgementInfo.CLEAR_POINT
+            if self.sent_miss == 0:
+                self.point += GameJudgementInfo.PERFECT_POINT
+            self.completed = True
+
+            return True
+        return False
+
+    def count_failure(self):
+        """
+        失敗をカウントする。
+
+        :return: なし
+        """
+        self.missed += 1
+        self.sent_miss += 1
+        self.point += GameJudgementInfo.MISS_POINT
+
+    def is_expected_key(self, code):
+        """
+        タイプされたキーが期待されているキーか確認する。
+
+        :param code: タイプされたキー
+        :return: 正しい場合はTrue
+        """
+
+        if len(self.target_roma) == 0:
+            return False
+
+        print(code, end="")
+
+        return self.target_roma[0] == code
--- Main.py
+++ Main.py
@@ -9,15 +9,13 @@ import pygame_misc
 
 import re
 
-ONE_CHAR_POINT = 10
-PERFECT_POINT = 100
-CLEAR_POINT = 50
-MISS_POINT = -30
-COULDNT_TYPE_POINT = -2
-
 pygame.mixer.pre_init(44100, 16, 2, 1024)
-pygame.init()
 pygame.mixer.init()
+pygame.init()
+
+
+from GameSystem import *
+
 
 class Score:
     LOG_ERROR = 1
@@ -54,36 +52,36 @@ def set_val_to_dictionary(dict, key, value):
 
 def read_score(file_name):
 
+    re_rect_blacket = re.compile(r"\[(.*)\]")
+
+    # ファイルを読み込む
     lines = []
     with open(file_name, mode="r") as f:
         lines = f.readlines()
 
+    # 読み込み用変数準備
     score = Score()
 
-    re_rect_blacket = re.compile(r"\[(.*)\]")
-
-    score_data = []
-    zone_data = {}
-    next_current_minute = 0
     current_minute = 0
-    current_seconds = 0.0
+    current_time = 0
 
     song = ""
     phon = ""
 
-    point = 0
-
     is_in_song = False
+
     for line in lines:
+
         line = line.strip()
 
-        # Comment
+        # ----- 処理対象行かの確認
+
+        # コメント
         if line.startswith("#"): continue
 
-        # Blank line
+        # 空行
         if len(line) == 0: continue
 
-        # Song property
         if line.startswith(":") and not is_in_song:
             line = line[1:]
             key, value = line.split()
@@ -109,17 +107,17 @@ def read_score(file_name):
         if rect_blk_match is not None:
             command = rect_blk_match[1]
             if command == "break":
-                score.score.append([60 * current_minute + current_seconds, "", ""])
+                score.score.append([current_time, "", ""])
                 continue
 
         if line.startswith(">>"):
-            score.score.append([60 * current_minute + current_seconds, line[2:], ""])
+            score.score.append([current_time, "", ""])
             continue
 
         # Minute
         if line.startswith("|"):
             line = line[1:]
-            next_current_minute = int(line)
+            current_minute = int(line)
             continue
 
         # Seconds
@@ -131,11 +129,12 @@ def read_score(file_name):
                 if len(phon) == 0:
                     score.log_error(line, "No pronunciation data!")
                     break
-                score.score.append([60 * current_minute + current_seconds, song, phon])
+                score.score.append([current_time, song, phon])
                 song = ""
                 phon = ""
-            current_seconds = float(line)
-            current_minute = next_current_minute
+
+            current_time = 60 * current_minute + float(line)
+
             continue
 
         if line.startswith("@"):
@@ -144,7 +143,7 @@ def read_score(file_name):
                 score.log_error(line, "Duplicated Section Name!")
                 break
             else:
-                set_val_to_dictionary(score.section, line, 60 * current_minute + current_seconds)
+                set_val_to_dictionary(score.section, current_time, line)
 
             continue
 
@@ -154,10 +153,10 @@ def read_score(file_name):
             flag, zone_name = line.split()
 
             if flag == "start":
-                score.zone.append([60 * current_minute + current_seconds, zone_name, "start"])
+                score.zone.append([current_time, zone_name, "start"])
                 continue
             elif flag == "end":
-                score.zone.append([60 * current_minute + current_seconds, zone_name, "end"])
+                score.zone.append([current_time, zone_name, "end"])
                 continue
 
         # Phonenics
@@ -182,102 +181,26 @@ def read_score(file_name):
 
 
 
-
 def main():
-
-    screen = pygame.display.set_mode((600, 480))
-    pygame.display.set_caption("Musical Typer")
-
-    nihongo_font = pygame.font.Font("mplus-1m-medium.ttf", 48)
-    alphabet_font = pygame.font.Font("mplus-1m-medium.ttf", 32)
-    full_font = pygame.font.Font("mplus-1m-medium.ttf", 24)
-    system_font = pygame.font.Font("mplus-1m-medium.ttf", 16)
 
     score_data = read_score("test_music_text.tsc")
 
-    count = 0
-    missed = 0
-
-    mainloop_continues = True
     pygame.mixer.music.play(1)
-    pygame.mixer.music.get_pos()
-
-    lyrincs_index = -1
-    zone_index = -1
-    lyrincs_index = -1
-    target_kana = ""
-    target_roma = ""
-    full = ""
-
-    point = 0
-    sent_miss = 0
-    sent_count = 0
-    completed = False
-
-    latest_missrate = 0
-    latest_missrate_col = (0, 0, 0)
-    missrate_col = (255, 0, 0)
 
     next_income = score_data.score[0][0]
 
-    current_zone = ""
+    progress = GameProgressInfo(score_data)
+    judge_info = GameJudgementInfo()
+    ui = Screen()
+
+    progress.lyrincs_index = -1
+
+    mainloop_continues = True
     while mainloop_continues:
 
-        pos = pygame.mixer.music.get_pos() / 1000
-
-        # If new lyncs coming
-        if score_data.score[lyrincs_index + 1][0] <= pos:
-
-            point += COULDNT_TYPE_POINT * len(target_roma)
-
-            lyrincs_index += 1
-            print(str(lyrincs_index) + " (" + str(score_data.score[lyrincs_index]) + ")")
-
-            next_income = score_data.score[lyrincs_index + 1][0]
-            full = score_data.score[lyrincs_index][1]
-            target_kana = score_data.score[lyrincs_index][2]
-            target_roma = Romautil.hira2roma(target_kana)
-            sent_count= 0
-            sent_miss = 0
-            completed = False
-            latest_missrate = missrate
-            latest_missrate_col = missrate_col
-            missrate = 0
-            missrate_col = (255, 0, 0)
-
-        if score_data.zone[zone_index + 1][0] <= pos:
-            zone_index += 1
-
-            if score_data.zone[zone_index][2] == "start":
-                current_zone = score_data.zone[zone_index][1]
-            else:
-                current_zone = ""
-
-        sentence_continues = True
-
-        screen.fill((0, 0, 0))
-
-
-        w, h = pygame.display.get_surface().get_size()
-        ratio = (next_income - pos) / (next_income - score_data.score[lyrincs_index][0])
-        missrate = sent_count / ((sent_miss + sent_count) if sent_miss + sent_count > 0 else 1)
-
-        pygame.draw.rect(screen, (128, 0, 0), (0, 0, math.floor(ratio * w), 120))
-
-        pygame_misc.print_str(screen, 5, 0, nihongo_font, target_kana)
-        pygame_misc.print_str(screen, 5, 55, full_font, full, (192, 192, 192))
-        pygame_misc.print_str(screen, 5, 80, alphabet_font, target_roma)
-        pygame_misc.print_str(screen, 5, 130, system_font, "Typed: {}".format(count))
-        pygame_misc.print_str(screen, 5, 150, system_font, "Miss: {}".format(missed))
-        pygame_misc.print_str(screen, 5, 170, system_font, "Sent: {}".format(sent_miss))
-        pygame_misc.print_str(screen, 5, 190, full_font, "Score: {}".format(point))
-        pygame_misc.print_str(screen, 5, 220, system_font, "Zone: {}".format(current_zone))
-        pygame_misc.print_str(screen, 5, 350, system_font, str(pos))
-
-        pygame.draw.rect(screen, missrate_col, (0, 85, math.floor(missrate * w), 2))
-        pygame.draw.rect(screen, latest_missrate_col, (0, 12, math.floor(latest_missrate * w), 2))
-        if completed:
-            pygame_misc.print_str(screen, 5, 280, full_font, "WA" if sent_miss != 0 else "AC", (255, 255, 120))
+        # --------------
+        #     Events
+        # --------------
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -286,41 +209,59 @@ def main():
             if event.type == KEYDOWN:
 
                 # Completed already, or no phonuciation provided
-                if len(target_roma) == 0:
+                if judge_info.completed:
                     continue
 
                 # filter event -- if alphabet, number, or "-" key pressed
                 if Romautil.is_readable_key_pressed(event.key):
                     # if correct key was pushed
-                    if target_roma[0] == chr(event.key):
-                        target_roma = target_roma[1:]
-                        target_kana = Romautil.get_not_halfway_hr(target_kana, target_roma)
-                        count += 1
-                        point += ONE_CHAR_POINT
-                        sent_count += 1
-
+                    if judge_info.is_expected_key(chr(event.key)):
+                        judge_info.count_success()
                     else:
-                        missed += 1
-                        sent_miss += 1
-                        point += MISS_POINT
-                        missrate = sent_miss / ((sent_miss + sent_count) if sent_miss + sent_count > 0 else 1)
-                        print(missrate)
-
-
-                    # Completed!
-                    if len(target_roma) == 0:
-                        point += CLEAR_POINT
-                        if sent_miss == 0:
-                            point += PERFECT_POINT
-                            missrate_col = (255, 255, 0)
-                        else:
-                            missrate_col = (0, 255, 0)
-                        completed = True
-
+                        judge_info.count_failure()
 
                 if event.key == K_ESCAPE:
                     mainloop_continues = False
                     break
+
+        # --------------------
+        #     Calculation
+        # --------------------
+
+        pos = pygame.mixer.music.get_pos() / 1000
+
+        current_lyrincs, index_changed = progress.get_current_lyrincs(pos)
+        # If new lyncs comingWDdd
+        if index_changed:
+
+            judge_info.point += GameJudgementInfo.COULDNT_TYPE_POINT * len(judge_info.target_roma)
+
+            judge_info.reset_sentence_score()
+            judge_info.set_current_lyrinc(score_data.score[progress.lyrincs_index][1], score_data.score[progress.lyrincs_index][2])
+
+
+            print()
+
+        ui.screen.fill((0, 0, 0))
+
+        w, h = pygame.display.get_surface().get_size()
+
+        pygame.draw.rect(ui.screen, (128, 0, 0), (0, 0, math.floor(progress.get_time_remain_ratio(pos) * w), 120))
+
+        ui.print_str(5, 0,   ui.nihongo_font,   judge_info.target_kana)
+        ui.print_str(5, 55,  ui.full_font,      judge_info.full, (192, 192, 192))
+        ui.print_str(5, 80,  ui.alphabet_font,  judge_info.target_roma)
+        ui.print_str(5, 130, ui.system_font,    "Typed: {}".format(judge_info.count))
+        ui.print_str(5, 150, ui.system_font,    "Miss: {}".format(judge_info.missed))
+        ui.print_str(5, 170, ui.system_font,    "Sent: {}".format(judge_info.sent_miss))
+        ui.print_str(5, 190, ui.full_font,      "Score: {}".format(judge_info.point))
+        ui.print_str(5, 220, ui.system_font,    "Zone: {}".format(progress.get_current_zone(pos)))
+
+        ui.print_str(5, 350, ui.system_font,    str(pos))
+
+        pygame.draw.rect(ui.screen, (255, 0, 0), (0, 85, math.floor(judge_info.get_sentence_missrate() * w), 2))
+        if judge_info.completed and judge_info.sent_count > 0:
+            ui.print_str(5, 280, ui.full_font, "WA" if judge_info.sent_miss != 0 else "AC", (255, 255, 120))
 
         # 60fps
         pygame.time.wait(1000 // 60)
