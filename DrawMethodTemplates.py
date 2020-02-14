3a62f59a78bd2a6a08d5e935ef81552c7c36d7f0
loxygenK <loxygen.k@gmail.com>
2020-02-14 13:16:19 +0900

    [Fix] fading

--- DrawMethodTemplates.py
+++ DrawMethodTemplates.py
@@ -1,13 +1,18 @@
 import pygame
 from GameSystem import Screen
 
+from ColorTheme import *
 
 def slide_fadeout_text(current_frame, total_frame, ui: Screen, args):
     w, h = ui.screen_size
 
-    color_r = args[1][0] - args[1][0] * (current_frame / total_frame)
-    color_g = args[1][1] - args[1][1] * (current_frame / total_frame)
-    color_b = args[1][2] - args[1][2] * (current_frame / total_frame)
+    color_r = args[1][0] + (BACKGROUND_COLOR[0] - args[1][0]) * (current_frame / total_frame)
+    color_g = args[1][1] + (BACKGROUND_COLOR[1] - args[1][1]) * (current_frame / total_frame)
+    color_b = args[1][2] + (BACKGROUND_COLOR[2] - args[1][2]) * (current_frame / total_frame)
+
+    color_r = max(0, min(color_r, 255))
+    color_g = max(0, min(color_g, 255))
+    color_b = max(0, min(color_b, 255))
 
     color = (color_r, color_g, color_b)
 
@@ -21,9 +26,13 @@ def slide_fadeout_text(current_frame, total_frame, ui: Screen, args):
 def blink_screen(current_frame, total_frame, ui: Screen, args):
     w, h = ui.screen_size
 
-    color_r = args[0][0] - args[0][0] * (current_frame / total_frame)
-    color_g = args[0][1] - args[0][1] * (current_frame / total_frame)
-    color_b = args[0][2] - args[0][2] * (current_frame / total_frame)
+    color_r = args[0][0] + (BACKGROUND_COLOR[0] - args[0][0]) * (current_frame / total_frame)
+    color_g = args[0][1] + (BACKGROUND_COLOR[1] - args[0][1]) * (current_frame / total_frame)
+    color_b = args[0][2] + (BACKGROUND_COLOR[2] - args[0][2]) * (current_frame / total_frame)
+
+    color_r = max(0, min(color_r, 255))
+    color_g = max(0, min(color_g, 255))
+    color_b = max(0, min(color_b, 255))
 
     color = (color_r, color_g, color_b)
 
