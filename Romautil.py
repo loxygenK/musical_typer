d81728f78141b06a52bb3053b21bc17d8d6df42e
loxygenK <loxygen.k@gmail.com>
2020-02-14 13:17:08 +0900

    [Fix] Small character

--- Romautil.py
+++ Romautil.py
@@ -51,6 +51,9 @@ def get_not_halfway_hr(full_hiragana, progress_roma):
     index = romaji.find(progress_roma)
 
     if re.match("[aeiouyn]", romaji[index]) and romkan.is_consonant(romaji[index - 1]):
-        return romkan.to_hiragana(romaji[index - 1:])
+        if len(romaji) >= 2 and romkan.is_consonant(romaji[index - 2]):
+            return romkan.to_hiragana(romaji[index - 2:])[1:]
+        else:
+            return romkan.to_hiragana(romaji[index - 1:])
 
     return romkan.to_hiragana(romaji[index:])
